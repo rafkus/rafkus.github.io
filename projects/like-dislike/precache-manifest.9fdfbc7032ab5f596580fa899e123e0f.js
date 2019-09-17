db0d0faa674bd68391f5ce24d0bb478abaa2c51e
anthropo5 <rk606797@gmail.com>
2019-09-17 17:47:36 +0200

    update

---
+++ projects/like-dislike/precache-manifest.9fdfbc7032ab5f596580fa899e123e0f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9947135fddaaad2b16f6555e9019ea18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1805db60552112647f5e",
+    "url": "./static/js/2.a439a3f4.chunk.js"
+  },
+  {
+    "revision": "c6510f8ff5023a311614",
+    "url": "./static/js/main.cab9a0e7.chunk.js"
+  },
+  {
+    "revision": "42846c0827f8f7de684a",
+    "url": "./static/js/runtime~main.00ec6bdf.js"
+  }
+]);

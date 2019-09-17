ca7851c339545598f3ff6f0c814d85185d45e182
anthropo5 <rk606797@gmail.com>
2019-09-17 17:44:17 +0200

    update

--- projects/slider/js/script.js
+++ projects/slider/js/script.js
@@ -3,10 +3,11 @@ import Slider from './Slider.js';
 const run = () => {
     const props = {
         container: "#slider",
-        imageSources: ["https://raw.githubusercontent.com/rafkus/rafkus.github.io/master/slider/assets/img1.jpg", 
-                       "https://raw.githubusercontent.com/rafkus/rafkus.github.io/master/slider/assets/img2.jpg", 
-                       "https://raw.githubusercontent.com/rafkus/rafkus.github.io/master/slider/assets/img3.jpg", 
-                       "https://raw.githubusercontent.com/rafkus/rafkus.github.io/master/slider/assets/img4.jpg"]
+        imageSources: [
+            "https://images.alphacoders.com/943/943148.jpg",
+            "https://images5.alphacoders.com/602/602575.png", 
+            "https://images.alphacoders.com/699/699287.jpg", 
+            "https://images4.alphacoders.com/220/220184.jpg"]
     }
 
     const slider = new Slider(props);

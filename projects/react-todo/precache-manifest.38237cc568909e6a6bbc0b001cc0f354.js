137b816c0f88c2f30e8192304e0c090e2c01173c
anthropo5 <rk606797@gmail.com>
2019-09-30 16:42:35 +0200

    add react-dish-form. add react-todo demo

---
+++ projects/react-todo/precache-manifest.38237cc568909e6a6bbc0b001cc0f354.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53b93703c6ee2bd6323ffb9f70634945",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b5f635d1fe0dd76fde5",
+    "url": "./static/js/2.b49ad58a.chunk.js"
+  },
+  {
+    "revision": "7522ae241ddc9cb12b1e",
+    "url": "./static/js/main.3109d0bb.chunk.js"
+  },
+  {
+    "revision": "2bf8c021f29fe22fd9f1",
+    "url": "./static/js/runtime-main.7a5f0e85.js"
+  }
+]);

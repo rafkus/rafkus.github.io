266df0214220694c5d37fc649e8b7d5bd0c14089
anthropo5 <rk606797@gmail.com>
2019-09-30 16:24:09 +0200

    deploy react-dishes-form with proper path

--- projects/react-dishes-form/service-worker.js
+++ projects/react-dishes-form/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e674595971c6ca3edc86fa439e3aa890.js"
+  "./precache-manifest.e52fafc823e83812f873f0bf0afa1733.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
